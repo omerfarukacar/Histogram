06e5277f028cbcb9973c44a2cb3667559be85298
Ömer Faruk ACAR <62004193+omerfarukacar@users.noreply.github.com>
2021-01-10 20:22:42 +0300

    Update 1.1

--- Histogram.py
+++ Histogram.py
@@ -1,3 +1,20 @@
+"""
+A histogram is an approximate representation of the distribution of numerical data.
+In this application calculate probability density function (PDF), cumulative distribution function (CDF) ,
+Estimated mean value, Real mean value , Error mean value,
+Estimated variance, Real variance, Error mean variance,
+Factorial, Moment vector, Derivative of moment vector and calculate of Taylor series.
+For more information please visit https://github.com/omerfarukacar/Huffman or https://github.com/oguzcantezel/Huffman
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Istanbul Commerce University Histogram Application
+%% -------------------
+%% $Author: Omer Faruk Acar, Oguzcan Tezel$
+%% $Date: January 9, 2021$,
+%% $Revision: 1.0$
+%% $Copyright: Ömer Faruk ACAR , Oğuzcan Tezel.$
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+"""
+
 import numpy as np
 
 NO_OF_BINS = 100  # Better performance needs higher [NO_OF_BINS]
@@ -26,7 +43,6 @@ for j in range(0, NO_OF_BINS):  # Iterate through the bins
             hist_count[j] = hist_count[j] + 1  # Construct the histogram
 
 pdf = hist_count / NO_OF_SAMPLES  # PDF is the normalized version of histogram
-
 cdf[0] = pdf[0]  # CDF, PDF integrated out initialized with zero starting from the first sample of PDF
 for k in range(1, NO_OF_BINS):  # Iterate through the bins
     cdf[k] = pdf[k] + cdf[k - 1]  # Integrate the PDF
@@ -40,7 +56,6 @@ for k in range(0, NO_OF_SAMPLES):  # Iterate through the random number array
     true_mean = true_mean + x_new[k] / float(len(x_new))  # Obtain the true mean
 
 estimated_variance = 0  # Initialized the variance storage
-
 for k in range(0, NO_OF_BINS):  # Iterate through the bins
     estimated_variance = estimated_variance + (t_new[k] ** 2) * pdf[k]  # Estimate the variance
 
@@ -56,26 +71,23 @@ true_x3 = 0  # Initialized the true x3 storage
 for k in range(0, NO_OF_SAMPLES):  # Iterate through the random number array
     true_x3 = true_x3 + (x_new[k] ** 3) / float(len(x_new))  # Obtain the true x3
 
-moment_array = np.zeros(NO_OF_MOMENTS)
-for m in range(0, NO_OF_MOMENTS):
-    for k in range(0, NO_OF_BINS):
-        moment_array[m] = moment_array[m] + (t_new[k] ** m) * pdf[k]
+moment_array = np.zeros(NO_OF_MOMENTS) #Initialized the moment_array
+for m in range(0, NO_OF_MOMENTS): # Iterate through the number of moments
+    for k in range(0, NO_OF_BINS): # Iterate through the number of bins
+        moment_array[m] = moment_array[m] + (t_new[k] ** m) * pdf[k] # Obtain the moment array
 
-new_array = np.zeros(NO_OF_MOMENTS)  #First degree derivative
-for t in range(0, NO_OF_MOMENTS - 1):
-    new_array[t] = moment_array[t + 1] - moment_array[t] #Derivative of the array
+derivative_array = np.zeros(NO_OF_MOMENTS)  #Initialized the derivative array
+for t in range(0, NO_OF_MOMENTS - 1): # Iterate through the number of moments - 1
+    derivative_array[t] = moment_array[t + 1] - moment_array[t] # Obtain the derivative array
 
-factorial_input = int(input("Enter number to calculate factorial:"))
-factorial_output = 0
-if not (factorial_input < 0):#Factorial must be greater than 0
-    factorial_output = 1#Give 1 if factorial is 0 or 1
-    for i in range(1, factorial_input+1):
-        factorial_output = factorial_output * i
+derivative_input = int(input("Enter number to calculate derivative array:")) # Derivative input degree is given by user
 
-derivative_input = int(input("Enter number to calculate derivative array:"))
-number_of_array_input = int(input("which number of array?:"))
 def n_th_order_difference(array, order): #Calculate nth order difference of given input
-
+    """
+    :param array:
+    :param order:
+    :return:
+    """
     length_of_input = len(array) # Check whether given order can be calculated or not
     if order >= length_of_input:  # Right hand side is 'm', thus order can be 'm-1' at max.
         raise ValueError("For an array with length 'm', order can not be greater than 'm-1'!")
@@ -85,9 +97,26 @@ def n_th_order_difference(array, order): #Calculate nth order difference of give
 
     diff = [array[i+1] - array[i] for i in range(length_of_input-1)]  # First order difference
     for n in range(order-1):  # Repeat the difference order-1 times to get nth order difference
-        diff = [diff[i+1] - diff[i] for i in range(len(diff) - 1)]
+        diff = [diff[i+1] - diff[i] for i in range(len(diff) - 1)] # Obtain the difference
     return diff
 
+def factorial(n): # Calculate factorial
+    result = 1 #Initialized the result
+    i = 0
+    if (n <= 1): # If factoial smaller and equal than 1 return 1
+        return 1
+    for i in range(2, n + 1):# Iterate through the n + 1
+        result = result * i
+    return result
+"""
+#üst hesaplamak için gerekli fonksiyon
+def myexp(x):
+  e=0
+  for i in range(0,100): #Sum the first 100 terms of the series
+    e = e+(x**i)/factorial(i)
+  return e
+"""
+# Print all result
 print("-" * 21,"REPORT", "-" * 21)
 print("-" * 50)
 print("NO_OF_BINS =", NO_OF_BINS)
@@ -112,14 +141,10 @@ print("Estimated x3 =", estimated_x3)
 print("true_x3 =", true_x3)
 print("Estimated x3 error = ", abs(estimated_x3 - true_x3))
 print("-" * 50)
-print("Factorial Output = ", factorial_output)
-print("-" * 50)
 print("moment_array =", moment_array)
-print("Moment array first derivative = ", new_array)
+print("Moment array first derivative = ", derivative_array)
 print("-" * 50)
 for n in range(derivative_input):
-    print("moment array ", number_of_array_input,  "value =", moment_array[number_of_array_input])
-    print("n_th_order_difference(moment_array, n)", number_of_array_input,  "value =", n_th_order_difference(moment_array, n)[number_of_array_input])
     print(f'Number of Derivative = {n}')
     print("Our Derivative =", n_th_order_difference(moment_array, n))
     print("Numpy test Derivative =", np.diff(moment_array, n))
